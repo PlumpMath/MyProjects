ec5b9398387b32efacf590e5a95d980dc4882ce7
Yu Sun <usunyu@gmail.com>
2016-03-03 01:12:16 -0800

    [cardboard] sphere rotation sample

---
+++ Virtual Reality/RotationPlanet/Assets/Scripts/CategoryScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CategoryScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGaze() {
+		Debug.Log ("StartGaze");
+	}
+
+	public void StopGaze() {
+		Debug.Log ("StopGaze");
+	}
+}
